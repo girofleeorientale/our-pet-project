3ae17b70fbb5503cbbbb4428523a60fd13e8e9db
girofleeorientale <42096601+girofleeorientale@users.noreply.github.com>
2020-12-23 02:15:52 +0300

    Add files via upload

---
+++ LetterIHorizontal.java
@@ -0,0 +1,49 @@
+public class LetterIHorizontal extends Shape {
+    int x;
+    int y;
+
+    public LetterIHorizontal() {
+
+    }
+
+    public LetterIHorizontal(Plateau p, int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean notOutOfBoundsUp (int x, int y) {
+        boolean res = true;
+        if (x < 0) {
+            res = false;
+        }
+//        System.out.println("notOutOfBoundsLeft " + res);
+        return res;
+    }
+
+    public boolean notOutOfBoundsDown (Plateau p, int x, int y) {
+        boolean res = true;
+        if (x >= p.hauteur-1) {
+            res = false;
+        }
+//        System.out.println("notOutOfBoundsRight " + res);
+        return res;
+    }
+
+    public boolean notOutOfBoundsLeft (int x, int y) {
+        boolean res = true;
+        if (y < 0) {
+            res = false;
+        }
+//        System.out.println("notOutOfBoundsUp " + res);
+        return res;
+    }
+
+    public boolean notOutOfBoundsRight (Plateau p, int x, int y) {
+        boolean res = true;
+        if (y >= p.largeur-3) {
+            res = false;
+        }
+//        System.out.println("notOutOfBoundsDown " + res);
+        return res;
+    }
+}
