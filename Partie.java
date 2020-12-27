fcf6e4ecc903ef13a7a6488734dd366119591c39
ZahraAll <76518577+ZahraAll@users.noreply.github.com>
2020-12-27 10:39:15 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -1,4 +1,6 @@
 
+
+  
 public class Partie {
 	Plateau p;
 	Joueur J;
@@ -12,31 +14,47 @@ public class Partie {
 	
 	public void Jouer() {
 	
-	p.init();
-    System.out.println("START");
-    System.out.println("Voilà le plateau, allons-y !");
-    p.placeShapes();
-   // p.displayValues();
-    System.out.println(" ");
-    p.displayColors();
-	int[] tab =J.demanderCoordonnes();//On demande les coordonnées de la case à supprimer;
-	p.eliminer_Voisines(tab[0],tab[1]);
-	p.use_tab();
-	p.reorganisation();	
-	/*p.goDown();
-    p.pushToLeft();*/
-    System.out.println("Après Supression");
-    p.displayColors();
-    System.out.println("Après réorganisation bas");
-    p.goDown();
-    p.displayColors();
-    System.out.println("Après réorganisation gauche"); 
-    p.pushToLeft();
-    p.displayColors();
-    
+		
+			
+			
+			J.demanderNom();
+		    System.out.println("START");
+		    System.out.println("Voilà le plateau, allons-y !");
+		    p.placeShapes();
+		   
+		    System.out.println(" ");
+		    p.init();
+		   
+		   
+		    p.displayColors();
+		    while(J.veutJouer())
+		    	
+			{p.reinit_tab();
+		    int[] tab =J.demanderCoordonnes();//On demande les coordonnées de la case à supprimer;
+			
+			p.eliminer_Voisines(tab[0],tab[1]);
+			for(int i=0;i<64;i++) {
+			p.use_tab();}
+			
+			p.reorganisation();	
+			
+		    System.out.println("Après Supression");
+		    p.displayColors();
+		    System.out.println("Après réorganisation bas");
+		    p.goDown();
+		    p.displayColors();
+		    System.out.println("Après réorganisation gauche"); 
+		    p.pushToLeft();
+		    p.displayColors();
+		    J.veutJouer();
+			}
+		    //if(p.IfAllSaved(2)) System.out.println("Yeah you winned !!!");
+		    
+		    p.jeuGagne(4);
+		    }
+			
     }
 	
 	
 	
 
-}
