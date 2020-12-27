4f4d61342afe9f41be6c194f41cc1b6177b8b639
girofleeorientale <42096601+girofleeorientale@users.noreply.github.com>
2020-12-27 19:32:35 +0300

    Add files via upload

---
+++ controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+//Controler le plateau;
+import model.Plateau;
+public class Controller {
+	 Plateau grille;
+	
+	public Controller(Plateau p) {
+		this.grille=p;
+	}
+
+}
---
+++ controller/GameEngine.java
@@ -0,0 +1,25 @@
+package controller;
+import java.awt.Event;
+import java.awt.*;
+import java.awt.Graphics;
+import model.Plateau;
+import view.SimplePlateauView;
+import view.Vue;
+
+public class GameEngine {
+/* Vue est une interface*/
+	private Plateau grille;
+	private Vue vue;
+	private Controller controleur;
+	
+	
+public  GameEngine() {
+	
+}
+
+
+
+
+
+
+}
