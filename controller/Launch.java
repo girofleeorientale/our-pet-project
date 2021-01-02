80a24b1e682c79128ec05380e4f63a9cd28c57ae
ZahraAll <76518577+ZahraAll@users.noreply.github.com>
2021-01-02 13:46:07 +0100

    Update Launch.java
    Ajout du bouton start

--- controller/Launch.java
+++ controller/Launch.java
@@ -1,17 +1,28 @@
 package controller;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.FileNotFoundException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import model.Plateau;
 import view.BoardView;
 import view.CellView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 
 public class Launch extends JFrame {
 	
@@ -24,14 +35,43 @@ public class Launch extends JFrame {
        
 		EventQueue.invokeLater(()-> {
 		Plateau p= new Plateau(8,8);
-                BoardView boardView = new BoardView(p, 1);
-            CellView cellView = new CellView(9, 9 , new Color(0,6,0));
-            Controller c= null;
+            BoardView boardView = new BoardView(p, 2);
+           // CellView cellView = new CellView(8, 8 , new Color(0,6,0));
+            JFrame frame = new JFrame();
+            JButton start = new JButton("start");
+            JPanel FirstPage= new JPanel();
+            FirstPage.add(start);
+            frame.add(FirstPage);
+            
+            
+            frame.setSize(500, 500);
+    		frame.setVisible(true);
+    		 start.addActionListener(new ActionListener()
+    		    {
+    		      public void actionPerformed(ActionEvent e)
+    		      {
+    		        
+    		        
+    		      
+    		      Controller c= null;
+    	            try {
+    	            	
+    	                c = new Controller(p,boardView);
+    	               
+    	                
+    	            } catch (FileNotFoundException e1) {
+    	                e1.printStackTrace();
+    	            }
+    		        
+    		      }
+    		    });
+            
+          /* Controller c= null;
             try {
                 c = new Controller(p, boardView);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
-            }
+            }*/
 
         });
     }
