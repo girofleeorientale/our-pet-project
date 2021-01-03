d7fbd4fe037006ba9ec278a60ebe91295992cffa
Anna Golikova <ann.golikova@gmail.com>
2021-01-03 22:47:48 +0300

    image background

---
+++ controller/BackgroundPanel.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+class BackgroundPanel extends JPanel
+{
+    Image image;
+    public BackgroundPanel()
+    {
+        try
+        {
+            image = ImageIO.read(new File("sea.png"));;
+        }
+        catch (Exception e) { /*handled in paintComponent()*/ }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        if (image != null)
+            g.drawImage(image, 0,0,this.getWidth(),this.getHeight(),this);
+    }
+}
